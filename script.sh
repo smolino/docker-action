cc7927c7d5c48ac0a9d299e8fca0eea6a314f9f6
Sergio Molino <sergio.molino@point2point.org.uk>
2022-10-23 20:42:05 +0200

    My 4first action is ready

--- script.sh
+++ script.sh
@@ -3,8 +3,7 @@ bash <(curl -L "https://get.entando.org/cli") --update --release="v7.1.1"
 source "$HOME/.entando/activate" --force
 ent check-env develop --yes
 source "$HOME/.entando/activate" --force
-podman ps
 git clone https://github.com/cecchisandrone/entando-simple-bundle.git
 cd entando-simple-bunle
-ent bundle init
+ent bundle init simple-bundle
 ent bundle pack
